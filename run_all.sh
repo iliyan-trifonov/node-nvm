d1caa52e71ba293f2a5341eb31b96c41fe0bb438
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2016-07-24 17:10:36 +0300

    temporary remove updating bower and forever on start

--- run_all.sh
+++ run_all.sh
@@ -17,7 +17,7 @@ fi
 
 #set the node version, update the global npm packages, install/update the app's npm and bower packages, run the app in production mode
 . ~/.nvm/nvm.sh && nvm use default; \
-  npm update -g bower forever --user "node"; \
+  #npm update -g bower forever --user "node"; \
   sudo chown -R node:node /myapp; \
   cd /myapp && npm update && bower install; \
   NODE_ENV=production forever $APP_MAIN
