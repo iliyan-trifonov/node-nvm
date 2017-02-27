e60c3738be93fdd689898e7bf6a0eab1c339d3d0
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2017-02-27 08:56:38 +0200

    skipped updating global bower and pm2 packages on start

--- run_all.sh
+++ run_all.sh
@@ -17,7 +17,7 @@ fi
 
 #set the node version, update the global npm packages, install/update the app's npm and bower packages, run the app in production mode
 . ~/.nvm/nvm.sh && nvm use default; \
-  npm update -g bower pm2 --user "node"; \
+  #npm update -g bower pm2 --user "node"; \
   sudo chown -R node:node /myapp; \
   cd /myapp && npm update && bower install; \
   NODE_ENV=production pm2 start $APP_MAIN -i 0
